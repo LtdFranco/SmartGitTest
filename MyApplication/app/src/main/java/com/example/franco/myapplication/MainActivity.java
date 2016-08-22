32afd4646ccca86198230a0c2dd646dc08c3e647
Franco <franco.liu@leediancn.com>
2016-08-22 14:43:16 +0800

    master version 1

---
+++ MyApplication/app/src/main/java/com/example/franco/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.franco.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText eText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        eText = (EditText) findViewById(R.id.GitTestText);
+        eText.setText("Test Rebase");
+
+    }
+}
